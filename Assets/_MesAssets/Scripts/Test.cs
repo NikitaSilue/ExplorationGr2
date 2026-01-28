14a7e0b2717465b16526cff7c031827acb074ade
NikitaSilue <siluenikita06@icloud.com>
2026-01-27 20:16:55 -0500

    ModifVoitures

--- Assets/_MesAssets/Scripts/Test.cs
+++ Assets/_MesAssets/Scripts/Test.cs
@@ -3,14 +3,8 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Test!!!!!!");
     }
 }
